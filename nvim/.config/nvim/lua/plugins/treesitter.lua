76c427d1042d225496be37a9bfadac4d495507dd
kwabenadarkwa <darkwak@live.com>
2026-03-31 16:36:54 +0100

    Revert "pin treesiter back to old version"
    This reverts commit cc3c01795689fb6f1a8a47c566642953711c01f0.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
-  commit = "3d0e642764f22d12c2496c27f132197f0ae96ccb",
   build = ":TSUpdate",
   event = { "BufNewFile", "BufReadPost" },
   ensure_installed = {
